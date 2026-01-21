3ada8ba1fc4b34cc07714ed450ed9ca889fbf3a9
MULLAIVENDAN22 <kmullai2223@gmail.com>
2026-01-21 17:44:26 +0530

    21/1/26 changes 1

--- template/App.jsx
+++ template/App.jsx
@@ -1,6 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import "./global.css";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import "./global.css"
+
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
@@ -14,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
